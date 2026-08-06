73bd47bb7a341ff9af8a116293f2998c320a5c89
Daniel-png06 <dabuh@byupathway.edu>
2026-08-06 12:02:50 +0100

    Fix Render deployment by removing CartSummary import

--- src/main.js
+++ src/main.js
@@ -3,18 +3,16 @@ import "./assets/css/main.css";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { Footer } from "./components/Footer";
-
 import { Home } from "./pages/Home";
-import { CartSummary } from "./components/CartSummary";
 
-document.querySelector("#app").innerHTML = `
+const app = document.querySelector("#app");
+
+app.innerHTML = `
     ${Navbar()}
 
     ${Hero()}
 
     ${Home()}
-
-    ${CartSummary()}
 
     ${Footer()}
 `;
